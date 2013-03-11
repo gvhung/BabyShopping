f625349b630b910f61dcef474d5f242a1e649a4d
nareshphuloria <naresh.phuloria@gmail.com>
2013-03-11 20:21:54 +0530

    User Model committed for Registration

--- Source Code/BabyShopping.Models/UserModel.cs
+++ Source Code/BabyShopping.Models/UserModel.cs
@@ -10,7 +10,15 @@ namespace BabyShop.Models
     public class UserModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [DisplayName("UserName")]
         public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [DisplayName("First Name")]
         public string Password { get; set; }
         
         [Required]
@@ -19,9 +27,19 @@ namespace BabyShop.Models
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email")]
         public string Email { get; set; }
+        
         public IList<UserAddressModel> UserAddressList { get; set; }
+        
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [DisplayName("Primary Phone")]
         public string PrimaryPhone { get; set; }
+        
         public string SecondaryPhone { get; set; }
         public string MobilePhone { get; set; }
         public string Fax { get; set; }
