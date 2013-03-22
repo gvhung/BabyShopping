ceb8cc6b6c2342157b014b186b9a081e42ccb0cb
nareshphuloria <naresh.phuloria@gmail.com>
2013-03-23 00:07:31 +0530

    The following changes were committed 1. Database back up 2. Field name changed in ProductModel 3. URL to redirect changed in product and inner layouts

--- Source Code/BabyShop.CommonLayer.DataTransferObjects/ProductDTO.cs
+++ Source Code/BabyShop.CommonLayer.DataTransferObjects/ProductDTO.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace BabyShop.CommonLayer.DataTransferObjects
+﻿namespace BabyShop.CommonLayer.DataTransferObjects
 {
     public class ProductDTO
     {
         public int Id { get; set; }
-
-        public string ProductName { get; set; }
-
+        public string Name { get; set; }
         public string Description { get; set; }
-
         public decimal Price { get; set; }
-
         public string ImageURL { get; set; }
     }
 }
--- Source Code/BabyShop.PersistenceLayer/ProductRepository.cs
+++ Source Code/BabyShop.PersistenceLayer/ProductRepository.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using BabyShop.CommonLayer.DataTransferObjects;
-
-using System.Linq;
-using System.Text;
 using BabyShop.CommonLayer.Utilities;
 using Microsoft.ApplicationBlocks.Data;
 
@@ -18,9 +14,8 @@ namespace BabyShop.PersistenceLayer
             IList<ProductDTO> productList = new List<ProductDTO>();
 
             using (IDataReader reader = SqlHelper.ExecuteReader(
-                                            ConnectionClass.OpenConnection(),
-                                            CommandType.StoredProcedure,
-                                            AppConstants.GETPRODUCTS))
+                ConnectionClass.OpenConnection(), CommandType.StoredProcedure, 
+                AppConstants.GETPRODUCTS))
             {
                 while (reader.Read())
                 {
@@ -33,13 +28,13 @@ namespace BabyShop.PersistenceLayer
         private static ProductDTO PopulateProduct(IDataRecord dataRecord)
         {
             return new ProductDTO
-                              {
-                                  Id = Convert.ToInt32(dataRecord["Id"]),
-                                  ProductName = Convert.ToString(dataRecord["ProductName"]),
-                                  Description = Convert.ToString(dataRecord["Description"]),
-                                  Price = Convert.ToDecimal(dataRecord["Price"]),
-                                  ImageURL = Convert.ToString(dataRecord["ImageURL"])
-                              };
+            {
+                Id = Convert.ToInt32(dataRecord["Id"]),
+                Name = Convert.ToString(dataRecord["Name"]),
+                Description = Convert.ToString(dataRecord["Description"]),
+                Price = Convert.ToDecimal(dataRecord["Price"]),
+                ImageURL = Convert.ToString(dataRecord["ImageURL"])
+            };
         }
     }
 }
