2a050a1892bfb05328ba93dc2d630e33dad97608
nareshphuloria <naresh.phuloria@gmail.com>
2013-03-23 21:35:19 +0530

    Source code committed

--- Source Code/BabyShopping.Models/ProductModel.cs
+++ Source Code/BabyShopping.Models/ProductModel.cs
@@ -3,13 +3,9 @@
     public class ProductModel
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public string Description { get; set; }
-
         public decimal Price { get; set; }
-
         public string ImageURL { get; set; }
     }
 }
